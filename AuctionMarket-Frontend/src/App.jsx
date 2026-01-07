46b5f862c8d75deb556b413d38ecf3aa5278fc7d
Haehun <jhh010143@gmail.com>
2026-01-07 21:43:40 +0900

    feat: 메인 페이지 배너 구현 (#3)

--- AuctionMarket-Frontend/src/App.jsx
+++ AuctionMarket-Frontend/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header.jsx';
+import MainPage from "./views/main-page/MainPage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <Router>
+            {/* 어떤 페이지를 가든 헤더는 항상 상단에 위치함 */}
+            <Header />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
