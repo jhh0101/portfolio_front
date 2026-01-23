0f54b3d43e8bc39e8a31a619936e318a6b54b971
Haehun <jhh010143@gmail.com>
2026-01-23 21:06:03 +0900

    refactor: 로그인 페이지 리팩토링
    - 로그인 훅 React Query로 변경
    - queryClient 전역 에러 핸들링

--- AuctionMarket-Frontend/src/App.jsx
+++ AuctionMarket-Frontend/src/App.jsx
@@ -1,33 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import {AuthProvider} from "./context/AuthContext.jsx";
 import Header from './components/header/Header.jsx';
+import Footer from "./components/footer/Footer.jsx";
 import MainPage from "./views/main-page/MainPage.jsx";
 import LoginPage from "./views/login-page/LoginPage.jsx";
-import {AuthProvider} from "./context/AuthContext.jsx";
 import SignupPage from "./views/signup-page/SignupPage.jsx";
 import ProductAddPage from "./views/product/product-add-page/ProductAddPage.jsx";
 import ProductPage from "./views/product/product-page/ProductPage.jsx";
-import Footer from "./components/footer/Footer.jsx";
 import PolicyPage from "./views/policy/PolicyPage.jsx";
 import ProductListPage from "./views/product/product-list-page/ProductListPage.jsx";
 
 function App() {
     return (
-        <AuthProvider>
-            <Router>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<SignupPage />} />
-                    <Route path="/product-add" element={<ProductAddPage />} />
-                    <Route path="/product/:productId" element={<ProductPage />} />
-                    <Route path="/shop" element={<ProductListPage />} />
-                    <Route path="/terms" element={<PolicyPage type={"terms"} />} />
-                    <Route path="/privacy" element={<PolicyPage type={"privacy"} />} />
-                </Routes>
-                <Footer />
-            </Router>
-        </AuthProvider>
+        <>
+            <Toaster position="top-center" reverseOrder={false} />
+
+            <AuthProvider>
+                <Router>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<MainPage />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/signup" element={<SignupPage />} />
+                        <Route path="/product-add" element={<ProductAddPage />} />
+                        <Route path="/product/:productId" element={<ProductPage />} />
+                        <Route path="/shop" element={<ProductListPage />} />
+                        <Route path="/terms" element={<PolicyPage type={"terms"} />} />
+                        <Route path="/privacy" element={<PolicyPage type={"privacy"} />} />
+                    </Routes>
+                    <Footer />
+                </Router>
+            </AuthProvider>
+        </>
     );
 }
 
