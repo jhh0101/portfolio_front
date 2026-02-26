7dcbd937641ac16776869298ad0cb46311c84d2d
Haehun <jhh010143@gmail.com>
2026-02-26 21:56:07 +0900

    fix: 사용자 정보 address 입력 추가 (#51)
    - 기본 주소는 카카오 우편번호 API로 필드 입력 가능
    - 상세 주소는 사용자가 직접 입력
    - 회원가입과 프로필 조회(수정) 페이지에 구현

--- AuctionMarket-Frontend/src/views/signup-page/SignupPage.jsx
+++ AuctionMarket-Frontend/src/views/signup-page/SignupPage.jsx
@@ -1,9 +1,12 @@
+import {useState} from 'react';
 import {useSignup} from "@/hooks/user";
 import AuthLayout from "@/components/common/layout/AuthLayout.jsx";
+import SearchAddress from "@/components/common/address/SearchAddress.jsx";
 import "./SignupPage.css";
 
 const SignupPage = () => {
     const { mutate, isPending } = useSignup();
+    const [baseAddress, setBaseAddress] = useState("");
 
     const handleSignup = async (e) => {
         e.preventDefault();
@@ -17,15 +20,30 @@ const SignupPage = () => {
                 <input type="email"
                        name="email"
                        required placeholder="Your email address" />
+
                 <input type="text"
                        name="username"
                        required placeholder="Name" />
+
                 <input type="text"
                        name="nickname"
                        required placeholder="Nickname" />
+                <div style={{display: "flex", alignItems: "center"}}>
+                    <input type="text"
+                           name="baseAddress"
+                           style={{flex: "7", marginRight: "6px"}}
+                           value={baseAddress || ""}
+                           required placeholder="Base Address" readOnly />
+                    <SearchAddress onSearch={setBaseAddress} />
+                </div>
+                    <input type="text"
+                           name="detailAddress"
+                           placeholder="Detail Address (e.g., 101호)" />
+
                 <input type="text"
                        name="phone"
                        required placeholder="Phone number" />
+
                 <input type="password"
                        name="password"
                        required placeholder="Password" />
